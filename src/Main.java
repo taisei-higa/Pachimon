3b10210a211fcfa4d87c7b10066c510d13f3f0c8
kokkuri81 <195759@ie.u-ryukyu.ac.jp>
2020-01-21 00:35:01 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,82 +1,81 @@
+
+
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
-import java.awt.*;
+import java.util.ArrayList;
 
 public class Main extends Application {
-    public static void main(String[] args)
-    {
-        launch(args);
-    }
-
-    String[] Map = {
-            "*_______________*",
-            "*_______________*",
-            "*_______________*",
-            "*_______________*",
-            "*______www______*",
-            "*______www______*",
-            "*______www______*",
-            "*______www______*",
-            "*______www______*",
-            "*_______________*",
-            "*_______________*",
-            "*_______________*",
-            "*_______________*",
-            "*_______________*"};
 
     @Override
-    public void start(Stage theStage)
-    {
-        theStage.setTitle( "pachimon" );
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("pachimon");
+        primaryStage.setScene(new Scene(root, 300, 275));
 
-        Group root = new Group();
-        Scene theScene = new Scene( root );
-        theStage.setScene( theScene );
+        Group host = new Group();
+        Scene theScene = new Scene(host);
+        primaryStage.setScene(theScene);
 
-        Canvas canvas = new Canvas( 900, 1000 );
-        root.getChildren().add( canvas );
+
+        Canvas canvas = new Canvas(1000, 1000);
+        host.getChildren().add(canvas);
+
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-
-        gc.setFill( Color.RED );
-        gc.setStroke( Color.BLACK );
-        gc.setLineWidth(2);
-        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
-        gc.setFont( theFont );
-
-        Image back = new Image( "back_green.jpg",1400,0,true,false );
-        gc.drawImage( back, 0, 0 );
-
-        gc.fillText( "Hello, World!", 63, 51 );
-        gc.strokeText( "Hello, World!", 60, 50 );
-
-        Image kusa = new Image("w.png",100, 0, true, false);
-        Image Wood = new Image("Wood.png", 100, 0, true, false);
-        for(int j= 0;j<Map.length;j++) {
-            for (int i = 0; i < Map[j].length(); i++) {
-                if (Map[j].charAt(i) == '*') {
-                    gc.drawImage(Wood, i * 50, j*50);
-                }
-                if (Map[j].charAt(i) == 'w') {
-                    gc.drawImage(kusa, i * 50, j*50);
-                }
-            }
-        }
-
-        theStage.show();
+        player player = new player();
+        player.player_Animation(gc);
 
 
-
+        primaryStage.show();
 
     }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
+
+
+
+class player{
+    public GraphicsContext player_Animation(GraphicsContext gc){
+        Image kusa = new Image("design_img_f_1555168_s.png", 10, 0, true, false);
+        final long startNanoTime = System.nanoTime();
+
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+
+                /*
+                現在はy(t)=Math.sin(t)となっている
+                xはx(t)=Math.sin(t)となっている
+                 */
+
+                double x = 500 + 250 * Math.cos(t);
+                double y = 500 + 250 * Math.sin(t);
+
+                // Clear the canvas
+                gc.clearRect(0, 0, 10000,10000);
+                // background image clears canvas
+                gc.drawImage( kusa, x, y );
+            }
+        }.start();
+        return gc;
+    }
+
+}
+
