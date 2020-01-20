c4199a4356a433423f363c181d70fa24bdac539d
kokkuri81 <195759@ie.u-ryukyu.ac.jp>
2020-01-21 00:57:55 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,22 @@
-
-
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.stage.Stage;
+import javafx.scene.input.KeyEvent;
 
 import java.util.ArrayList;
 
+
 public class Main extends Application {
+    int x = 0;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -26,19 +29,23 @@ public class Main extends Application {
         primaryStage.setScene(theScene);
 
 
-        Canvas canvas = new Canvas(1000, 1000);
+        Canvas canvas = new Canvas(900, 1000);
         host.getChildren().add(canvas);
 
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        player player = new player();
-        player.player_Animation(gc);
+
+        player p = new player();
+        p.player_Animation(gc);
+
+
 
 
         primaryStage.show();
 
     }
+
 
 
     public static void main(String[] args) {
@@ -47,31 +54,69 @@ public class Main extends Application {
 }
 
 
+class Map {
+    String[] Map = {
+            "*_______________*",
+            "*_______________*",
+            "*_______________*",
+            "*_______________*",
+            "*______www______*",
+            "*______www______*",
+            "*______www______*",
+            "*______www______*",
+            "*______www______*",
+            "*_______________*",
+            "*_______________*",
+            "*_______________*",
+            "*_______________*",
+            "*_______________*"
+
+    };
+
+    public GraphicsContext criateMap(GraphicsContext gc) {
+        Image kusa = new Image("w.png", 100, 0, true, false);
+        Image Wood = new Image("Wood.png", 100, 0, true, false);
+        for (int j = 0; j < Map.length; j++) {
+            for (int i = 0; i < Map[j].length(); i++) {
+                if (Map[j].charAt(i) == '*') {
+                    gc.drawImage(Wood, i * 50, j * 50);
+                }
+                if (Map[j].charAt(i) == 'w') {
+                    gc.drawImage(kusa, i * 50, j * 50);
+                }
+            }
+        }
+        return gc;
+    }
+}
+
+
+
 
 
 class player{
     public GraphicsContext player_Animation(GraphicsContext gc){
-        Image kusa = new Image("design_img_f_1555168_s.png", 10, 0, true, false);
+        Image kusa = new Image("w.png", 100, 0, true, false);
         final long startNanoTime = System.nanoTime();
 
         new AnimationTimer()
         {
             public void handle(long currentNanoTime)
             {
-                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+                double elapsedTime = (currentNanoTime - startNanoTime) / 1000000000.0;
 
-                /*
-                現在はy(t)=Math.sin(t)となっている
-                xはx(t)=Math.sin(t)となっている
-                 */
+                ArrayList<String> input = new ArrayList<String>();
 
-                double x = 500 + 250 * Math.cos(t);
-                double y = 500 + 250 * Math.sin(t);
+
+                double x = 232 + 128 * Math.cos(elapsedTime);
+                double y = 232 + 128 * Math.sin(elapsedTime);
 
                 // Clear the canvas
                 gc.clearRect(0, 0, 10000,10000);
                 // background image clears canvas
                 gc.drawImage( kusa, x, y );
+                Map a=new Map();
+                a.criateMap(gc);
             }
         }.start();
         return gc;
